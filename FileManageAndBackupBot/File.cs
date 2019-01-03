8f1b25a5d1c31fabdf60b2fd7cf303c07b15ae16
Jan Petersen <jdpdo@outlook.de>
2019-01-03 15:04:20 +0100

    Setting up project files and basic structure of core.

---
+++ FileManageAndBackupBot/File.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IO = System.IO;
+
+namespace FileManageAndBackupBot
+{
+    public class File
+    {
+        private Uri uri;
+
+        public Uri Uri { get { return uri; } set { uri = value; } }
+
+        public File(string uri)
+        {
+            
+        }
+    }
+}
