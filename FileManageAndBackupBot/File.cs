975dfc48b193f2ac3d995247b583efdcb3b4277c
Jan Petersen <jdpdo@outlook.de>
2019-01-09 09:55:58 +0100

    Added methods MoveFile & ChangeFileName to File, add submodule xunit.xUnit and allowed unsafe code for it.

--- FileManageAndBackupBot/File.cs
+++ FileManageAndBackupBot/File.cs
@@ -9,11 +9,61 @@ namespace FileManageAndBackupBot
     {
         private Uri uri;
 
-        public Uri Uri { get { return uri; } set { uri = value; } }
+        public Uri Uri { get { return uri; }
+            set
+            {
+                if (uri.IsFile)
+                {
+                    uri = value;
+                }
+            }
+        }
 
         public File(string uri)
         {
-            
+            Uri _uri = new Uri(uri);
+            if (_uri.IsFile) this.uri = _uri;
+        }
+
+        public bool MoveFile(string destFileName)
+        {
+            Uri newUri = new Uri(this.uri.AbsolutePath + destFileName);
+            try
+            {
+                IO.File.Move(uri.AbsoluteUri, newUri.AbsoluteUri);
+            }
+            catch (IO.IOException e)
+            {
+                Console.Error.WriteLine(
+                    "{0}: The write operation could not be performed" +
+                    "because the specified part of the file is not avaiable.",
+                    e.GetType().Name);
+                return false;
+                throw e;
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("{0}: An error occured",
+                    e.GetType().Name);
+                return false;
+                throw e;
+            }
+            return true;
+        }
+
+        public bool ChangeFileName(string destFileName)
+        {
+            Uri newUri = new Uri(this.uri.AbsolutePath + destFileName);
+            try
+            {
+                MoveFile(newUri.AbsoluteUri);
+            }
+            catch (Exception e)
+            {
+                return false;
+                throw e;
+            }
+            return true;
         }
     }
 }
