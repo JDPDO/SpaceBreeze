d34f0fcc259e3c777f2aeff55caba3cc2c703142
Jan Petersen <jdpdo@outlook.de>
2019-02-08 23:31:01 +0100

    Delete DirectoryNode class. Add methods GetSubFileTrees, GetChildren, GetSubDirectories, GetSubFiles and SearchChildByName to FileTree class. And do several improvements to FileTree class.

--- FileManageAndBackupBot/FileTree.cs
+++ FileManageAndBackupBot/FileTree.cs
@@ -5,50 +5,29 @@ using IO = System.IO;
 
 namespace FileManageAndBackupBot
 {
+    /// <summary>
+    /// Represents a tree with a root directoy and subordinated files and directories.
+    /// </summary>
     public class FileTree
     {
-        public class DirectoryNode
-        {
-            private LocalDirectory directory;
-            private List<File> childFiles;
-            private List<LocalDirectory> childDirs;
-
-            public LocalDirectory Directory
-            {
-                get => directory;
-            }
-
-            public File[] ChildFiles
-            {
-                get => childFiles.ToArray();
-            }
-
-            public DirectoryNode(LocalDirectory directory)
-            {
-                this.directory = directory;
-                string[] children = directory.GetChildrenNames();
-
-                foreach(string child in children)
-                {
-                    Uri uri = new Uri(directory.FullName + child);
-                    if (uri.IsFile) childFiles.Add(new File(uri));
-                    else childDirs.Add((LocalDirectory)Activator.CreateInstance(typeof(LocalDirectory), uri));
-                }
-            }
-        }
-
-        //DirectoryNode startNode;
-        LocalDirectory startLocalDirectory;
-        int layer;
+        // The root directory.
+        private IO.DirectoryInfo startDirectory;
+        // The current layer of main FileTree object.
+        private int layer;
+        // List of child roots.
+        private List<FileTree> subTrees;
 
         /// <summary>
         /// Creates a new file tree object using custom root driectory object.
         /// </summary>
-        /// <param name="directory">The managing directory object of root directory.</param>
-        public FileTree(LocalDirectory directory)
+        /// <param name="startDirectory">The managing directory object of root directory.</param>
+        public FileTree(IO.DirectoryInfo startDirectory)
         {
-            startLocalDirectory = directory;
+            this.startDirectory = startDirectory;
+            subTrees = new List<FileTree>();
             layer = 0;
+
+            Initialize();
         }
 
         /// <summary>
@@ -57,34 +36,94 @@ namespace FileManageAndBackupBot
         /// <param name="path">The path of custom root directory in file system.</param>
         public FileTree(string path)
         {
-            startLocalDirectory = (LocalDirectory)Activator.CreateInstance(typeof(LocalDirectory), path);
+            startDirectory = (IO.DirectoryInfo)Activator.CreateInstance(typeof(IO.DirectoryInfo), path);
+            subTrees = new List<FileTree>();
             layer = 0;
+
+            Initialize();
         }
 
         /// <summary>
         /// Creats a new file tree object with given path string and layer since custom root directory.
         /// </summary>
-        /// <param name="path">The path of directory in file system.</param>
+        /// <param name="startDirectory">The path of directory in file system.</param>
         /// <param name="layer">The layer since custom root directory.</param>
-        private FileTree(string path, int layer)
+        private FileTree(IO.DirectoryInfo startDirectory, int layer)
         {
-            startLocalDirectory = (LocalDirectory)Activator.CreateInstance(typeof(LocalDirectory), path);
+            this.startDirectory = startDirectory;
+            subTrees = new List<FileTree>();
             this.layer = layer;
+
+            Initialize();
         }
+
+        /// <summary>
+        /// Initializes the current FileTree object recursively.
+        /// </summary>
+        private void Initialize()
+        {
+            foreach(var directory in startDirectory.GetDirectories())
+            {
+                subTrees.Add(new FileTree(directory, layer + 1));
+            }
+        }
+
+        public FileTree[] GetSubFileTrees() => subTrees.ToArray();
+
+        /// <summary>
+        /// Returns all child elements of the current layer.
+        /// </summary>
+        /// <returns>Returns FileSystemInfo object array.</returns>
+        public IO.FileSystemInfo[] GetChildren() => startDirectory.GetFileSystemInfos();
+
+        /// <summary>
+        /// Returns all directory elements of the current layer.
+        /// </summary>
+        /// <returns>Returns DirectoyInfo object array.</returns>
+        public IO.DirectoryInfo[] GetSubDirectories() => startDirectory.GetDirectories();
+
+        /// <summary>
+        /// Returns all file elements of the current layer.
+        /// </summary>
+        /// <returns>Returns FileInfo object array.</returns>
+        public IO.FileInfo[] GetSubFiles() => startDirectory.GetFiles();
+
+        /// <summary>
+        /// Searches recursively for all files and directories with the given name.
+        /// </summary>
+        /// <param name="fileName">Name of the element to be searched.</param>
+        /// <returns>First found file object or if no object was found null.</returns>
+        public IEnumerable<IO.FileSystemInfo> SearchChildByName(string fileName)
+        {
+            // List with elements for the search.
+            List<IO.FileSystemInfo> children = new List<IO.FileSystemInfo>(GetChildren());
+            // List with found elements.
+            List<IO.FileSystemInfo> foundElements = new List<IO.FileSystemInfo>();
+            // The compare item.
+            IO.FileSystemInfo searchObject = new IO.FileInfo(fileName);
+            // If object in {children} found its index.
+            int index = children.BinarySearch(searchObject);
+            // Check if index is object and adds it to {foundElements}.
+            if (index >= 0) foundElements.Add(children[index]);
+            // Get matching objects of all following {subTrees}.
+            foreach (var subTree in subTrees)
+            {
+                foundElements.AddRange(subTree.SearchChildByName(fileName));
+            }
+            return foundElements;
+        }
+
+
 
         /// <summary>
         /// Prints all subfiles and subdirectories of current one hierarchically in output console.
         /// </summary>
         public void PrintFileTree()
         {
-            IO.FileSystemInfo[] children = startLocalDirectory.GetChildren();
-            foreach (var child in children)
+            foreach (var child in subTrees)
             {
-                PrintLine(layer, child.Name);
-                if (child.Extension == String.Empty)
-                {
-                    new FileTree(child.FullName, layer + 1).PrintFileTree();
-                }
+                PrintLine(layer, child.startDirectory.Name);
+                child.PrintFileTree();
             }
         }
 
@@ -95,7 +134,7 @@ namespace FileManageAndBackupBot
         /// <param name="text">Text to write in the output console.</param>
         private void PrintLine(int preTabs, string text)
         {
-            string tabs = String.Concat(System.Linq.Enumerable.Repeat("\t", preTabs));
+            string tabs = String.Concat(System.Linq.Enumerable.Repeat("  ", preTabs));
             Console.WriteLine(tabs + text);
         }
     }
