a8565cd17fde0ad1fdba80d830b962c7837e9035
Jan Petersen <jdpdo@outlook.de>
2019-02-08 15:06:24 +0100

    Add File, FileClient and FtpDirectory class.

---
+++ FileManageAndBackupBot/FileClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentFTP;
+
+namespace FileManageAndBackupBot
+{
+    class FileManageClient
+    {
+        // private fields
+        private FtpDirectory ftpDirectory;
+        private LocalDirectory rootDirectory;
+        private Queue<File> fileQueue;
+        private Queue<LocalDirectory> directoryQueue;
+
+
+        /// <summary>
+        /// File control manager. Managing local and remote files using the libary.
+        /// </summary>
+        public FileManageClient(string localRoot, string host, int port, string username, string password)
+        {
+            fileQueue = new Queue<File>();
+            ftpDirectory = new FtpDirectory(host, port, username, password);
+            rootDirectory = new LocalDirectory(localRoot);
+            //ftpDirectory.
+        }
+    }
+}
