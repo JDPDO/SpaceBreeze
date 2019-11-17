8eaf28548175fc6652665b0bffabbaeee426e51c
Jan Petersen <jdpdo@outlook.de>
2019-11-17 10:02:59 +0100

    Add property 'Exceptions', a stack for exceptions given to 'ExceptionHandler' class, and method 'GetLastException' to 'ExceptionHandler'.

--- SpaceBreeze/ExceptionHandler.cs
+++ SpaceBreeze/ExceptionHandler.cs
@@ -7,6 +7,8 @@ namespace JDPDO.SpaceBreeze
 {
     public static class ExceptionHandler
     {
+        public static Stack<Exception> Exceptions { get; private set; }
+
         /// <summary>
         /// Logs a thrown exception to standard error stream.
         /// </summary>
@@ -15,6 +17,7 @@ namespace JDPDO.SpaceBreeze
         {
             string output = exception.GetType().ToString() + " in " + exception.Source + ". Message:" + exception.Message;
             Console.Error.WriteLine(output);
+            Exceptions.Push(exception);
         }
 
         /// <summary>
@@ -27,5 +30,16 @@ namespace JDPDO.SpaceBreeze
             Exception outerException = new Exception(message, innerException);
             LogException(outerException);
         }
+
+        /// <summary>
+        /// Returns last object pushed to exception stack.
+        /// </summary>
+        /// <returns>Null if exception stack is empty.</returns>
+        public static Exception GetLastException()
+        {
+            try { return Exceptions.Peek(); }
+            catch (InvalidOperationException e) { LogException("Exception stack is empty.", e); }
+            return null;
+        } 
     }
 }
