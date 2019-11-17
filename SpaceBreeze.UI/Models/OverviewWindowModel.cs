f2cb2fe5be1039dbbf57baf37dfe8f63fa65d7e5
Jan Petersen <jdpdo@outlook.de>
2019-11-17 22:26:02 +0100

    Make Changes to 'OverviewWindowModel': - Add exception handling to methods: 'RegisterInstance', 'ProvideInstance', 'RemoveInstance' and 'OpenUplinkWindowAsync' - Make 'OpenUplinkWindow' async and improve code quality.

--- SpaceBreeze.UI/Models/OverviewWindowModel.cs
+++ SpaceBreeze.UI/Models/OverviewWindowModel.cs
@@ -18,7 +18,7 @@ namespace JDPDO.SpaceBreeze.UI.Models
         /// <summary>
         /// Contains the app's instance register.
         /// </summary>
-        private InstanceRegister register;
+        private readonly InstanceRegister register;
 
         /// <summary>
         /// Contains the app's configuration.
@@ -51,7 +51,7 @@ namespace JDPDO.SpaceBreeze.UI.Models
         {
             var subRegister = register.GetSubRegister(type);
             return subRegister;
-        } 
+        }
 
         /// <summary>
         /// Registers an instance.
@@ -59,7 +59,24 @@ namespace JDPDO.SpaceBreeze.UI.Models
         /// <param name="type"></param>
         /// <param name="title"></param>
         /// <param name="instance"></param>
-        public void RegisterInstance(string type, string title, object instance) => register.RegisterInstance(type, title, instance);
+        public bool RegisterInstance(string type, string title, object instance)
+        {
+            bool success = false;
+            try
+            {
+                register.RegisterInstance(type, title, instance);
+                success = true;
+            }
+            catch (ArgumentNullException e)
+            {
+                ExceptionHandler.LogException("Instance and/or type parameter are null.", e);
+            }
+            catch (ArgumentException e)
+            {
+                ExceptionHandler.LogException("Id may not be null.", e);
+            }
+            return success;
+        }
 
         /// <summary>
         /// Provides an instance with given type and unique title.
@@ -67,7 +84,22 @@ namespace JDPDO.SpaceBreeze.UI.Models
         /// <param name="type"></param>
         /// <param name="title"></param>
         /// <returns>Requested object from subregister or if nothing found null.</returns>
-        public object ProvideInstance(string type, string title) => register.ProvideInstance(type, title);
+        public object ProvideInstance(string type, string title)
+        {
+            try
+            {
+                return register.ProvideInstance(type, title);
+            }
+            catch (ArgumentNullException e)
+            {
+                ExceptionHandler.LogException("Type or titel are null.", e);
+            }
+            catch (KeyNotFoundException e)
+            {
+                ExceptionHandler.LogException("The property is retrieved. Type and/or id does not exist in the subregister.", e);
+            }
+            return null;
+        }
 
         /// <summary>
         /// Removes an instance from the registers.
@@ -75,9 +107,22 @@ namespace JDPDO.SpaceBreeze.UI.Models
         /// <param name="type"></param>
         /// <param name="title"></param>
         /// <returns>True if success, else false.</returns>
-        public bool RemoveInstance(string type, string title) => register.RemoveInstance(type, title);
+        public bool RemoveInstance(string type, string title)
+        {
+            bool success = false;
+            try
+            {
+                register.RemoveInstance(type, title);
+                success = true;
+            }
+            catch (ArgumentNullException e)
+            {
+                ExceptionHandler.LogException("Type or titel are null.", e);
+            }
+            return success;
+        }
 
-        public async void OpenUplinkWindow(string title, BrowserWindow mainWindow = null, BrowserWindowOptions options = null)
+        public async Task<bool> OpenUplinkWindowAsync(string title, BrowserWindow mainWindow = null, BrowserWindowOptions options = null)
         {
             // Checkout main window.
             mainWindow = mainWindow ?? Electron.WindowManager.BrowserWindows.First();
@@ -90,28 +135,40 @@ namespace JDPDO.SpaceBreeze.UI.Models
                     Show = false,
                 };
             }
-            // Start creating browser window.
-            //Task<BrowserWindow> createWindow = Electron.WindowManager.CreateWindowAsync(options);
-            BrowserWindow window = await Electron.WindowManager.CreateWindowAsync(options);
-            window.OnReadyToShow += () => window.Show();
-            // Set up protocol manager and save it in registers.
-            UplinkModel uplink = (UplinkModel)register.ProvideInstance("uplink", title);
-            IProtocolManager protocolManager;
-            if (!register.InstanceExists(uplink.Type, title))
+
+            // Start creating window.
+            Task<BrowserWindow> createWindow = Electron.WindowManager.CreateWindowAsync(options);
+            try
             {
-                // Init remote manager.
-                protocolManager = new FtpsRemoteManager(uplink.Host, uplink.Host.UrlGetPort(), uplink.User, uplink.Password);
-                register.RegisterInstance(uplink.Type, title, protocolManager);
-                // Init root directory.
-                ManagedDirectory rootDirectory = protocolManager.GetWorkingDirectory();
-                register.RegisterInstance("rootDirectory", title, rootDirectory);
+                /* Check if wanted uplink was activated at runtime yet. */
+                UplinkModel uplink = (UplinkModel)register.ProvideInstance("uplink", title);
+                if (!register.InstanceExists(uplink.Type, title))
+                {
+                    // Init remote manager.
+                    IProtocolManager protocolManager = new FtpsRemoteManager(uplink.Host, uplink.Host.UrlGetPort(), uplink.User, uplink.Password);
+                    register.RegisterInstance(uplink.Type, title, protocolManager);
+                    // Init root directory.
+                    ManagedDirectory rootDirectory = protocolManager.GetWorkingDirectory();
+                    register.RegisterInstance("rootDirectory", title, rootDirectory);
+                }
             }
-            //else protocolManager = (IProtocolManager)register.ProvideInstance(uplink.Type, title);
-            // Open browser window.
-            //BrowserWindow window = await createWindow;
-            //window.OnReadyToShow += () => window.Show();
-            //window.SetParentWindow(mainWindow);
+            catch (ArgumentNullException e) { ExceptionHandler.LogException("Instance and/or type is null.", e); }
+            catch (ArgumentException e) { }
+            catch (InvalidOperationException e) { }
+
+            /* Create window. */
+            BrowserWindow window = await createWindow;
+            // End here if creation was not successful.
+            if (createWindow.IsFaulted)
+            {
+                ExceptionHandler.LogException("The window could not be created.", createWindow.Exception);
+                return false;
+            }
             window.LoadURL(@"/uplink/index/{title}?type={type}");
+            window.OnReadyToShow += () => window.Show();
+            window.SetParentWindow(mainWindow);
+
+            return true;
         }
     }
 }
