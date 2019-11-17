c1b77797607f620deee40b02f091676aee5eca49
Jan Petersen <jdpdo@outlook.de>
2019-11-17 09:34:34 +0100

    Comment class 'InstanceRegister' out and remove its exception handling.

--- SpaceBreeze/InstanceRegister.cs
+++ SpaceBreeze/InstanceRegister.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace JDPDO.SpaceBreeze
 {
@@ -30,24 +31,34 @@ namespace JDPDO.SpaceBreeze
         /// </summary>
         private Dictionary<string, Dictionary<string, object>> registers;
 
-        public InstanceRegister(IEnumerable<KeyValuePair<string, Dictionary<string, object>>> collection)
+        /// <summary>
+        /// Initialize new instance register with predefined collection.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Dictionary contains one or more duplicate keys.</exception>
+        /// <param name="collection">A predefined collection.</param>
+        public InstanceRegister(IEnumerable<KeyValuePair<string, Dictionary<string, object>>> dictionary)
         {
             // Init register with collection if given or not if not given.
-            registers = (collection != null)
-                ? new Dictionary<string, Dictionary<string, object>>(collection) 
+            registers = (dictionary != null)
+                ? new Dictionary<string, Dictionary<string, object>>(dictionary) 
                 : new Dictionary<string, Dictionary<string, object>>();
 
             // Declare first InstanceRegister to Type if none declared before.
             if (firstInstanceRegister == null) firstInstanceRegister = this;
         }
 
+        /// <summary>
+        /// Creates a new empty instance register.
+        /// </summary>
         public InstanceRegister() : this(null) { }
 
         /// <summary>
         /// Returns all subregisters for given types.
         /// </summary>
         /// <param name="type">To returning registers of types or type.</param>
-        /// <returns>NUll if none subregister was found.</returns>
+        /// <exception cref="ArgumentNullException">Type is null.</exception>
+        /// <exception cref="KeyNotFoundException">The property is retrieved and type does not exist in the subregister.</exception>
+        /// <returns>Empty subregister if none matching type was found.</returns>
         public Dictionary<string, object> GetSubRegister(string type)
         {
             return registers.ContainsKey(type) ? registers[type] : new Dictionary<string, object>();
@@ -59,24 +70,20 @@ namespace JDPDO.SpaceBreeze
         /// <param name="type">The instance type of the object to be registered.</param>
         /// <param name="id">A id for the instance to be registered.</param>
         /// <param name="instance">The instance to be registered.</param>
+        /// <exception cref="ArgumentException">Id is null.</exception>
+        /// <exception cref="ArgumentNullException">Instance and/or type is null.</exception>
         public void RegisterInstance(string type, string id, object instance)
         {
+
             if (instance != null)
             {
-                try
+                if (!registers.ContainsKey(type))
                 {
-                    if (!registers.ContainsKey(type))
-                    {
-                        registers.Add(type, new Dictionary<string, object>());
-                    }
-                    registers[type].Add(id, instance);
+                    registers.Add(type, new Dictionary<string, object>());
                 }
-                catch (Exception e)
-                {
-                    ExceptionHandler.LogException(e);
-                }
+                registers[type].Add(id, instance);
             }
-            else ExceptionHandler.NewArgumentNullException(nameof(instance), "The Object have to be initialized.");
+            else throw new ArgumentNullException(nameof(instance), "The Object have to be initialized.");
         }
 
         /// <summary>
@@ -84,49 +91,28 @@ namespace JDPDO.SpaceBreeze
         /// </summary>
         /// <param name="type">The instance type of the object to be provided.</param>
         /// <param name="id">The id of the instance to be provided.</param>
+        /// <exception cref="ArgumentNullException">One or multiple arguments are empty or null.</exception>
+        /// <exception cref="KeyNotFoundException">The property is retrieved. Type and/or id does not exist in the subregister.</exception>
         /// <returns>The instance to given id.</returns>
-        public object ProvideInstance(string type, string id)
-        {
-            try
-            {
-                if (!(String.IsNullOrEmpty(type) && String.IsNullOrEmpty(id)))
-                {
-                    return registers[type][id];
-                }
-            }
-            catch (Exception e)
-            {
-                ExceptionHandler.LogException(e);
-            }
-            // If an argument equals null.
-            ExceptionHandler.NewArgumentNullException(nameof(id), "The id value have to be defined.");
-            return null;
-        }
+        public object ProvideInstance(string type, string id) => registers[type][id];
 
         /// <summary>
         /// Removes an instance of the register.
         /// </summary>
         /// <param name="type"></param>
         /// <param name="id"></param>
-        /// <returns>True if removing succeed, else false.</returns>
-        public bool RemoveInstance(string type, string id)
-        {
-            try { return registers[type].Remove(id); }
-            catch (Exception e) { ExceptionHandler.LogException(e); }
-            return false;
-        }
+        /// <exception cref="ArgumentNullException">Type and/or id is null.</exception>
+        /// <exception cref="KeyNotFoundException">The property is retrieved and type does not exist in the collection.</exception>
+        public void RemoveInstance(string type, string id) => registers[type].Remove(id);
 
         /// <summary>
         /// Checks if instance exists.
         /// </summary>
         /// <param name="type">Type of checked instance.</param>
         /// <param name="id">Identifier of instance.</param>
-        /// <returns></returns>
-        public bool InstanceExists(string type, string id)
-        {
-            try { return registers[type].ContainsKey(id); }
-            catch (Exception e) { ExceptionHandler.LogException(e); }
-            return false;
-        }
+        /// <exception cref="ArgumentNullException">Type and/or id is null.</exception>
+        /// <exception cref="KeyNotFoundException">The property is retrieved and type does not exist in the collection.</exception>
+        /// <returns>True if instance of object is avaiable. False if not.</returns>
+        public bool InstanceExists(string type, string id) => registers[type].ContainsKey(id);
     }
 }
